cdc5ce7c0ef8116966f27efaff0112089060addc
1-cool <38838272+1-cool@users.noreply.github.com>
2019-05-30 12:58:29 +0800

    Create ready.sh

---
+++ ready.sh
@@ -0,0 +1,14 @@
+######################################################################
+#                            准备Linux安装脚本
+######################################################################
+
+#!/bin/bash
+#准备SS脚本
+wget https://raw.githubusercontent.com/1-cool/Linux/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+#准备网络加速脚本
+wget https://raw.githubusercontent.com/dlxg/Linux-NetSpeed/master/tcp.sh
+chmod +x tcp.sh
+#准备SSH端口更改脚本
+wget https://raw.githubusercontent.com/1-cool/Linux/master/ssh-port.sh
+chmod +x ssh-port.sh
