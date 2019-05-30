c1b383ecc06aed16c03661bcad36aad78fff6694
1-cool <38838272+1-cool@users.noreply.github.com>
2019-05-30 13:37:44 +0800

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -4,20 +4,17 @@
 
 #!/bin/bash
 
-red='\033[0;31m'
-plain='\033[0m'
-
-#检查权限
-[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 需要root权限执行此脚本!" && exit 1
-
-#准备SS脚本
+#准备SS/SSR安装脚本
 wget https://raw.githubusercontent.com/1-cool/Linux/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 
-#准备网络加速脚本
+#准备网络加速安装脚本
 wget https://raw.githubusercontent.com/dlxg/Linux-NetSpeed/master/tcp.sh
 chmod +x tcp.sh
 
 #准备SSH端口更改脚本
 wget https://raw.githubusercontent.com/1-cool/Linux/master/ssh-port.sh
 chmod +x ssh-port.sh
+
+#准备宝塔安装脚本
+wget http://download.bt.cn/install/install-ubuntu_6.0.sh
