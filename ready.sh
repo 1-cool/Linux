d3cbf5db35b00ba66cb4908c70e677432f2fb58a
1-cool <38838272+1-cool@users.noreply.github.com>
2019-07-31 13:32:35 +0800

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -9,7 +9,7 @@ wget https://raw.githubusercontent.com/1-cool/Linux/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 
 #准备网络加速安装脚本
-wget https://raw.githubusercontent.com/dlxg/Linux-NetSpeed/master/tcp.sh
+wget https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh
 chmod +x tcp.sh
 
 #准备SSH端口更改脚本
