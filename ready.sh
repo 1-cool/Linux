500277e11b721e5a9ea98887faa9121532132d8c
1-cool <38838272+1-cool@users.noreply.github.com>
2019-09-19 12:29:28 +0800

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -13,8 +13,8 @@ wget https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh
 chmod +x tcp.sh
 
 #准备SSH端口更改脚本
-wget https://raw.githubusercontent.com/1-cool/Linux/master/ssh-port.sh
-chmod +x ssh-port.sh
+#wget https://raw.githubusercontent.com/1-cool/Linux/master/ssh-port.sh
+#chmod +x ssh-port.sh
 
 #准备宝塔安装脚本
 wget http://download.bt.cn/install/install-ubuntu.sh
