6a16124164263403922b2333dd6e791b5c5b6567
1-cool <38838272+1-cool@users.noreply.github.com>
2020-02-04 19:46:19 +0800

    Update view-connections.sh

--- view-connections.sh
+++ view-connections.sh
@@ -1,9 +1,12 @@
 ############################################################################
 #                             查看v2ray连接列表
 ############################################################################
+#读取v2ray端口
 PORT=$(grep \"port /etc/v2ray/config.json | tr -cd "[0-9]")
+#读取连接列表
 LIST=$(lsof -i -n -P | grep ESTABLISHED | grep $PORT | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u)
 for IP in $LIST
 do
+        #查询IP归属地
         curl https://ip.cn/index.php?ip=$IP
 done
