eba4b342bdff3ea55ea288e5c2d845026d2617d7
1-cool <38838272+1-cool@users.noreply.github.com>
2020-02-04 19:36:50 +0800

    Update view-connections.sh

--- view-connections.sh
+++ view-connections.sh
@@ -1,6 +1,9 @@
 ############################################################################
 #                             查看v2ray连接列表
 ############################################################################
-#!/bin/bash
-port=$(grep \"port /etc/v2ray/config.json | tr -cd "[0-9]")
-lsof -i -n -P | grep ESTABLISHED | grep ${port} | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u
+PORT=$(grep \"port /etc/v2ray/config.json | tr -cd "[0-9]")
+LIST=$(lsof -i -n -P | grep ESTABLISHED | grep $PORT | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u)
+for IP in $LIST
+do
+        curl https://ip.cn/index.php?ip=$IP
+done
