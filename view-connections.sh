a87d096258a9c347b20a46974c5faa85cd094a5d
1-cool <38838272+1-cool@users.noreply.github.com>
2020-02-04 18:45:36 +0800

    Update view-connections.sh

--- view-connections.sh
+++ view-connections.sh
@@ -3,4 +3,4 @@
 ############################################################################
 #!/bin/bash
 port=$(grep \"port /etc/v2ray/config.json | tr -cd "[0-9]")
-lsof -i -n -P | grep ESTABLISHED | grep ${port}
+lsof -i -n -P | grep ESTABLISHED | grep ${port} | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u
