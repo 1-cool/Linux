531fb34cb8eeebe1fa925ea8fab44f0ed16c5f40
1-cool <38838272+1-cool@users.noreply.github.com>
2020-02-05 16:22:55 +0800

    updata

--- view-connections.sh
+++ view-connections.sh
@@ -4,14 +4,14 @@
 #读取v2ray端口
 PORT=$(grep \"port /etc/v2ray/config.json | tr -cd "[0-9]")
 #读取连接列表
-LIST=$(lsof -i -n -P | grep ESTABLISHED | grep $PORT | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u)
-for IP in $LIST
+LIST=$(lsof -i -n -P | grep ESTABLISHED | grep ${PORT} | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | sort -u)
+for IP in ${LIST}
 do
         #显示连接v2ray的IP
-        echo $IP
+        echo ${IP}
 done
-for IP in $LIST
+for IP in ${LIST}
 do
         #查询IP归属地
-        curl https://ip.cn/index.php?ip=$IP
+        curl https://ip.cn/index.php?ip=${IP}
 done
