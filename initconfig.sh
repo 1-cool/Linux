ef8daf72e839b4142091d2a30bd5f85d8cd456ef
1-cool <38838272+1-cool@users.noreply.github.com>
2020-02-28 21:54:19 +0800

    update

---
+++ initconfig.sh
@@ -0,0 +1,10 @@
+######################################################################################################
+#                                        系统：debian
+######################################################################################################
+
+#!/bin/sh
+# 禁止SSH密码登录
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
+# 安装mtr
+sudo apt install -y mtr
