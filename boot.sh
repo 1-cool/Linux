8028b35733ef2d295c452d26bb706342f8df4670
1-cool <38838272+1-cool@users.noreply.github.com>
2020-04-22 23:54:22 +0800

    add boot.sh and ready.sh

---
+++ boot.sh
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                        系统：debian
+######################################################################################################
+
+#!/bin/sh
+# 禁止SSH密码登录
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
+# 下载准备脚本
+cd /root/
+wget https://raw.githubusercontent.com/1-cool/Linux/master/ready.sh
+bash ready.sh
+# 安装mtr
+sudo apt install -y mtr
---
+++ ready.sh
@@ -0,0 +1,27 @@
+######################################################################
+#                            准备Linux安装脚本
+######################################################################
+
+#!/bin/bash
+
+#准备SS/SSR安装脚本
+#wget https://raw.githubusercontent.com/1-cool/Linux/master/shadowsocks-all.sh
+#chmod +x shadowsocks-all.sh
+
+#准备v2ray安装脚本
+wget https://git.io/v2ray.sh
+chmod +x v2ray.sh
+
+#准备网络加速安装脚本
+wget https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh
+chmod +x tcp.sh
+
+#准备SSH端口更改脚本
+#wget https://raw.githubusercontent.com/1-cool/Linux/master/ssh-port.sh
+#chmod +x ssh-port.sh
+
+#准备宝塔安装脚本
+wget http://download.bt.cn/install/install-ubuntu_6.0.sh
+
+#查看v2ray连接列表脚本
+wget https://raw.githubusercontent.com/1-cool/Linux/master/view-connections.sh
