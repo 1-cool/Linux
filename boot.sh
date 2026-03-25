a67f61ffb6d70c6f630e92adc984753534019c15
1-cool <38838272+1-cool@users.noreply.github.com>
2026-03-25 14:38:22 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -12,3 +12,7 @@ curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/inst
 
 # 安装和配置fail2ban
 curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
+
+# 安装谷歌浏览器
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
