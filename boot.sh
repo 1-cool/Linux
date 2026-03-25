1716b9c9f6f45a690c142f5a03a87cb007fa7391
1-cool <38838272+1-cool@users.noreply.github.com>
2026-03-25 14:34:18 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 ######################################################################################################
-#                                        系统：debian
+#                                        系统：Ubuntu
 ######################################################################################################
 
 
-# 禁止SSH密码登录
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
-sed -i 's/#LogLevel INFO/LogLevel INFO/g' /etc/ssh/sshd_config
-echo "Protocol 2" &>> /etc/ssh/sshd_config
-sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/g' /etc/ssh/sshd_config
-systemctl restart sshd.service
-# 下载准备脚本
-cd /root/
-wget https://raw.githubusercontent.com/1-cool/Linux/master/ready.sh
-bash ready.sh
-# 安装mtr screen ftp
-sudo apt install -y mtr screen ftp
+# 加固SSH的安全
+curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh | bash
+
+# 安装zsh和oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh | bash
+
+# 安装和配置fail2ban
+curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
