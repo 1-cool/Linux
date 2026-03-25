32767948d1e4481ea5ef9bfa65c270c65ddbd2c5
1-cool <38838272+1-cool@users.noreply.github.com>
2026-03-25 14:41:41 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -15,4 +15,4 @@ curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail
 
 # 安装谷歌浏览器
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
