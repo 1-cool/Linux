939223746307ab507c5ae8db8efd5453b6d9a925
1-cool <38838272+1-cool@users.noreply.github.com>
2026-03-31 11:15:59 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,20 +5,24 @@
 
 
 # 加固SSH的安全
-curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh | bash
+# curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh | bash
+curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh | bash
 
 # 安装zsh和oh-my-zsh
-curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh | bash
+#curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh | bash
+curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh | bash
 
 # 安装和配置fail2ban
-curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
+#curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
+curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
 
 # 安装谷歌浏览器
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 
 # 下载.vimrc
-wget -O ~/.vimrc https://raw.githubusercontent.com/1-cool/Linux-config/refs/heads/master/.vimrc
+#wget -O ~/.vimrc https://raw.githubusercontent.com/1-cool/Linux-config/refs/heads/master/.vimrc
+wget -O ~/.vimrc https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux-config/refs/heads/master/.vimrc
 mkdir -p $HOME/.vim/backup/
 mkdir -p $HOME/.vim/swap/
 mkdir -p $HOME/.vim/undo/
