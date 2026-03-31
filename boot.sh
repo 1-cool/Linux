c8a9aef0be21042e19bc8df91d2da2779ac6ecd8
1-cool <38838272+1-cool@users.noreply.github.com>
2026-03-31 11:58:03 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,19 +5,15 @@
 
 
 # 加固SSH的安全
-# curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh | bash
-wget https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh
-bash ssh-safe.sh
+# bash <(curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh)
+bash <(curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/ssh-safe.sh)
 
 # 安装zsh和oh-my-zsh
-#curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh | bash
-wget https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh
-bash install-zsh.sh
-
+# bash <(curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh)
+bash <(curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/install-zsh.sh)
 # 安装和配置fail2ban
-#curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh | bash
-wget https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh
-bash fail2ban.sh
+# bash <(curl -fsSL https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh)
+bash <(curl -fsSL https://gh-proxy.org/https://raw.githubusercontent.com/1-cool/Linux/refs/heads/master/fail2ban.sh)
 
 # 安装谷歌浏览器
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
